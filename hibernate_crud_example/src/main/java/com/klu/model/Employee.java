8957cb65db4eee9254d16c00b5d164199427ee45
deepamrutha <2400031965@kluniversity.in>
2026-01-21 10:12:38 +0530

    Project added on 21-01-2026

---
+++ hibernate_crud_example/src/main/java/com/klu/model/Employee.java
@@ -0,0 +1,58 @@
+package com.klu.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "emp")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "emp_id")
+    private int empId;
+
+    @Column(name = "emp_name")
+    private String empName;
+
+    @Column(name = "salary")
+    private double salary;
+
+    @ManyToOne
+    @JoinColumn(name = "dept_id")   
+    private Department department;
+
+    public Employee() {
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+}
